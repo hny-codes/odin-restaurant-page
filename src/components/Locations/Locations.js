e85e8d1e288c8716b2168e06f964080be9771df7
Henry <hnguyhenry@protonmail.com>
2023-03-01 20:23:19 -0800

    Add location component to page

---
+++ src/components/Locations/Locations.js
@@ -0,0 +1,64 @@
+import styles from './locations.module.css';
+
+import PageHero from '../PageHero/PageHero';
+import siteImage from '../../../images/site/location-1.jpg';
+
+const Locations = () => {
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('wrapper');
+
+  const Hero = () => {
+    const heroText = 'Locations';
+    return PageHero(siteImage, heroText);
+  };
+
+  const LocationItem = (title, text) => {
+    const item = document.createElement('div');
+    item.classList.add(styles.location);
+
+    // Location elements
+    const locationName = document.createElement('h3');
+    const locationStreet = document.createElement('p');
+    const locationCityStateZip = document.createElement('p');
+    const locationNumber = document.createElement('p');
+    const locationAvailability = document.createElement('p');
+
+    // Location details
+    locationName.textContent = title;
+    locationStreet.textContent = text;
+    locationCityStateZip.textContent = `City, State, 90000`;
+    locationNumber.textContent = `CALL OR TEXT: (123) 456-7890`;
+    locationAvailability.textContent = `OPEN 7 DAYS A WEEK | 8:00AM - 10:00 PM`;
+
+    item.append(
+      locationName,
+      locationStreet,
+      locationCityStateZip,
+      locationNumber,
+      locationAvailability
+    );
+    return item;
+  };
+
+  const LocationBody = () => {
+    const bodyLocation = document.createElement('div');
+    bodyLocation.classList.add(styles['body-container']);
+
+    const bodyTitle = document.createElement('h2');
+    bodyTitle.textContent = "Come say hello! We won't bite!";
+    bodyLocation.appendChild(bodyTitle);
+
+    // Location elements
+    bodyLocation.append(
+      LocationItem('Location 1', '1234 Example St'),
+      LocationItem('Location 2', '4321 Example St')
+    );
+    return bodyLocation;
+  };
+
+  wrapper.appendChild(Hero());
+  wrapper.appendChild(LocationBody());
+  return wrapper;
+};
+
+export default Locations;
